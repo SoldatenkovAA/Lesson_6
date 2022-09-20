76d59b9b6f5095527c3e29c2b4c9ff150f20a39a
Andrey Soldatenkov <soldatenkov067@bk.ru>
2022-09-20 16:07:16 +0300

    add solution of the task_15 with else if

--- Examples/Homework_2/Task_15/Program.cs
+++ Examples/Homework_2/Task_15/Program.cs
@@ -4,9 +4,8 @@
 7 -> да
 1 -> нет     */
 
-Console.WriteLine("Введите цифру от 1 до 7, обозначающую день недели");
-int dayNumber = Convert.ToInt32(Console.ReadLine());
 
+/* Придумал 2 варианта функции - один через else if, второй -  в if положил if & else - оба рабочие
 void checkDayOff(int number)    
 {
     if (number > 0 && number <8)  
@@ -19,6 +18,25 @@ void checkDayOff(int number)
         {
             Console.WriteLine("Да, этот день - выходной");
         }
+    }
+    else 
+    {
+        Console.WriteLine("Нужно ввести число от 1 до 7, попробуйте еще раз");
+    }
+}  */
+
+Console.WriteLine("Введите цифру от 1 до 7, обозначающую день недели");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void checkDayOff(int number)    
+{
+    if (number > 0 && number <6)  
+    {
+        Console.WriteLine("Нет, этот день не является выходным");
+    }
+    else if (number > 5 && number <8)
+    {
+        Console.WriteLine("Да, этот день - выходной");
     }
     else 
     {
