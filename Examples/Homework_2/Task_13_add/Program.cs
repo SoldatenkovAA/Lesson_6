cdb62ce7a70a24ce81a5f80fc078507d275fe3a4
Andrey Soldatenkov <soldatenkov067@bk.ru>
2022-09-20 22:17:40 +0300

    add task_13_add with negative numbers

---
+++ Examples/Homework_2/Task_13_add/Program.cs
@@ -0,0 +1,27 @@
+﻿// вариант с отрицательными числами
+int randomNumber = new Random().Next(-1000000, 1000000);
+Console.WriteLine($"Случайное число {randomNumber}");
+
+if(randomNumber < 0)
+{
+    randomNumber = - randomNumber;
+}
+
+int searchThirdDigit (int number)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    number = number % 10;
+    return number;
+}
+if (randomNumber > 99)
+{
+    int thirdDigit = searchThirdDigit(randomNumber);
+    Console.WriteLine($"Третья цифра данного числа {thirdDigit}");
+}
+else 
+{
+    Console.WriteLine($"У числа {randomNumber} третьей цифры нет");
+}
