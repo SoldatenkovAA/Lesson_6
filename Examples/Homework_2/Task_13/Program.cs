f26d8a4692fa9a3bbed2d9846a2e292086602ba9
Andrey Soldatenkov <soldatenkov067@bk.ru>
2022-09-20 21:52:59 +0300

    add task_13 in homework_2

--- Examples/Homework_2/Task_13/Program.cs
+++ Examples/Homework_2/Task_13/Program.cs
@@ -1,7 +1,27 @@
-﻿/*
-Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
  что третьей цифры нет.
 645 -> 5
 78 -> третьей цифры нет
-32679 -> 6
-*/
+32679 -> 6 */
+
+int randomNumber = new Random().Next(1, 1000000);
+Console.WriteLine($"Случайное число {randomNumber}");
+
+int searchThirdDigit (int number)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    number = number % 10;
+    return number;
+}
+if (randomNumber > 99)
+{
+    int thirdDigit = searchThirdDigit(randomNumber);
+    Console.WriteLine($"Третья цифра данного числа {thirdDigit}");
+}
+else 
+{
+    Console.WriteLine($"У числа {randomNumber} третьей цифры нет");
+}
