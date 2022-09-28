ff57a3cc7a72a1f359559e01113ad50215081af7
Andrey Soldatenkov <soldatenkov067@bk.ru>
2022-09-28 14:55:58 +0300

    add task 29 from homework_4

--- Examples/Homework_4/Task_29/Program.cs
+++ Examples/Homework_4/Task_29/Program.cs
@@ -3,5 +3,31 @@
 5 -> [1, 2, 5, 7, 19]
 3 -> [6, 1, 33]     */  
 
-// при желании можно добавить сортировку массива от мах к мин и наоборот (если будет желание)
-// можно копирнуть - задача такая уже была, если нужно от мах к мин - нажмите У или Х например
+void printColorText(string information, ConsoleColor color)    // функция добавления цвета в шрифт
+{
+    Console.ForegroundColor = color;
+    Console.Write(information);
+    Console.ResetColor();
+}
+
+printColorText("Введите длину массива:  ", ConsoleColor.DarkGreen);
+int lengthMassive = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [lengthMassive];
+
+void randomDigitInArray(int [] array)  //функция заполнения рандомного заполнения массива в диапазоне [1,99]
+{
+    int i = 0;
+    for(i = 0; i < lengthMassive - 1; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        printColorText($"{array[i]}, ", ConsoleColor.DarkCyan);
+    }
+    array[i] = new Random().Next(1, 100);
+    printColorText($"{array[i]}", ConsoleColor.DarkCyan);
+}
+
+printColorText("[", ConsoleColor.DarkMagenta);
+randomDigitInArray(array);
+printColorText("]", ConsoleColor.DarkMagenta);
+Console.WriteLine();
